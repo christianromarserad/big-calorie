61c5d34a74d46dba6924dab6d5b1acccd6116a47
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-14 15:17:30 -0400

    Add a user sidebar

---
+++ src/components/User/UserSideBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserSideBar(props) {
+    return (
+        <div class="bg-white w-64 h-full shadow-lg">
+            <p>Praise the Lord</p>
+        </div>
+    )
+}
+
+export default UserSideBar;
