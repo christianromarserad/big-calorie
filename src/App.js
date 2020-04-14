71b037d35a22a87a4a0891882330e65be96b342f
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-14 15:19:08 -0400

    Set up the components in App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { incrementCountActionCreator } from './store/CounterReducer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MealPlanPage from './components/MealPlan/MealPlanPage';
+import MealPlanEditPage from './components/MealPlan/MealPlanEditPage';
+import UserSideBar from './components/User/UserSideBar';
 
 function App(props) {
   return (
-    <div>
-      <MealPlanPage />
+    <div class="flex bg-gray-200 h-screen">
+      <div>
+        <UserSideBar />
+      </div>
+      <div class="flex-1">
+        <Router>
+          <Switch>
+            <Route path="/edit/:day/:index" component={MealPlanEditPage} />
+            <Route path="/" component={MealPlanPage} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
