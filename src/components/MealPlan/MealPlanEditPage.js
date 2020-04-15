2c643632d4ef3a1d7e26348c89dd7dc5a84493c5
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-15 10:31:28 -0400

    Improve UI in MealPlanEditPage

--- src/components/MealPlan/MealPlanEditPage.js
+++ src/components/MealPlan/MealPlanEditPage.js
@@ -5,6 +5,12 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import {
+    getTotalCalories,
+    getTotalCarbs,
+    getTotalFat,
+    getTotalProtein
+} from '../../utils/Nutrients';
+import {
     updateMealNameActionCreator,
     addFoodActionCreator
 } from '../../store/MealPlanReducer';
@@ -41,62 +47,110 @@ function MealPlanEditPage(props) {
 
 
     return (
-        <div class="m-10 relative">
-            <Link to={"/" + props.selectedDay + "/" + props.index}>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-2 rounded-lg">
-                    back
-                </button>
-            </Link>
-            <input
-                class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
-                value={props.mealName}
-                onChange={props.updateMealNameActionCreator.bind(this, day, index)} />
+        <div class="w-full p-10 h-full">
+            <div class="bg-white p-10 rounded-lg shadow-lg h-full">
+                <div class="h-full flex flex-col">
+                    <Link to={"/" + props.selectedDay + "/" + props.index}>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-2 rounded-lg">
+                            back
+                        </button>
+                    </Link>
 
-            <input
-                class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
-                value={searchKeyWord}
-                onBlur={searchOnFocusOut}
-                onChange={searchOnChange} />
+                    <div class="mt-8 mb-16 flex items-center">
+                        <div class="w-full flex-1 mr-2">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                Meal Name
+                            </label>
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-last-name"
+                                type="text"
+                                placeholder="Doe"
+                                value={props.mealName}
+                                onChange={props.updateMealNameActionCreator.bind(this, day, index)} />
+                        </div>
 
-            {
-                searchItems.length != 0 ?
-                    <div class="bg-white rounded-b-md absolute w-full shadow-lg h-64 overflow-y-scroll">
+                        <div class="flex-1 flex  ml-2 text-center">
+                            <p class="flex-1 font-bold text-gray-700 text-base">
+                                Calories: {getTotalCalories(props.foods.map(food => (food.calorie)))}
+                            </p>
+                            <p class="flex-1 font-bold text-gray-700 text-base">
+                                Carb: {getTotalCarbs(props.foods.map(food => (food.carb)))}
+                            </p>
+                            <p class="flex-1 font-bold text-gray-700 text-base">
+                                Protein: {getTotalProtein(props.foods.map(food => (food.protein)))}
+                            </p>
+                            <p class="flex-1 font-bold text-gray-700 text-base">
+                                Fat: {getTotalFat(props.foods.map(food => (food.fat)))}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="relative">
+                        <input
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            type="text"
+                            placeholder="Search"
+                            value={searchKeyWord}
+                            onBlur={searchOnFocusOut}
+                            onChange={searchOnChange} />
+
                         {
-                            searchItems.map(({ food_name, serving_qty, serving_unit, photo }) => (
-                                <button
-                                    class="block w-full hover:bg-gray-200 font-bold py-2 px-4 border flex items-center"
-                                    onMouseDown={props.addFoodActionCreator.bind(this, day, index, food_name)}>
+                            searchItems.length != 0 ?
+                                <div class="bg-white rounded-b-md absolute w-full shadow-lg h-64 overflow-y-scroll">
+                                    {
+                                        searchItems.map(({ food_name, serving_qty, serving_unit, photo }) => (
+                                            <button
+                                                class="block w-full hover:bg-gray-200 font-bold py-2 px-4 border flex items-center"
+                                                onMouseDown={props.addFoodActionCreator.bind(this, day, index, food_name)}>
 
-                                    <img class="object-contain h-10 w-20" src={photo.thumb} />
-                                    <p class="text-gray-600 flex-1 text-right">{food_name}</p>
-                                    <p class="text-gray-600 flex-1 text-right">{serving_qty} {serving_unit}</p>
-                                </button>
-                            ))
+                                                <img class="object-contain h-10 w-20" src={photo.thumb} />
+                                                <p class="text-gray-600 flex-1 text-right">{food_name}</p>
+                                                <p class="text-gray-600 flex-1 text-right">{serving_qty} {serving_unit}</p>
+                                            </button>
+                                        ))
+                                    }
+                                </div> : null
                         }
-                    </div> : null
-            }
-            <div>
-                <table class="table-auto overflow-hidden w-full">
-                    <tbody>
-                        {
-                            props.foods.map(({ foodName, calorie, carb, protein, fat }) => {
-                                return (
-                                    <tr>
-                                        <td class="px-4 py-2">{foodName}</td>
-                                        <td class="px-4 py-2 text-xs">carb: {carb}</td>
-                                        <td class="px-4 py-2 text-xs">protein: {protein}</td>
-                                        <td class="px-4 py-2 text-xs">fat: {fat}</td>
-                                        <td class="px-4 py-2 text-xs">calorie: {calorie}</td>
-                                    </tr>
-                                );
-                            })
-                        }
-                    </tbody>
-                </table>
+                    </div>
+
+                    <div class="flex-1 flex flex-col overflow-hidden">
+                        <div class="w-full mt-6">
+                            <div class="flex text-center font-medium text-gray-600 bg-teal-300 rounded-md">
+                                <div class="px-4 py-2 w-7/12">Photo</div>
+                                <div class="px-4 py-2 w-7/12">Name</div>
+                                <div class="px-4 py-2 w-7/12">Carb</div>
+                                <div class="px-4 py-2 w-7/12">Protein</div>
+                                <div class="px-4 py-2 w-7/12">Fat</div>
+                                <div class="px-4 py-2 w-7/12">Calorie</div>
+                                <div class="px-4 py-2 w-7/12">Delete</div>
+                            </div>
+                        </div>
+
+                        <div class="flex-1 overflow-y-scroll">
+                            {
+                                props.foods.map(({ foodName, calorie, carb, protein, fat, photo }) => {
+                                    return (
+                                        <div class="bg-teal-100 text-center flex mt-5 mb-5 rounded-lg font-bold text-gray-700 border border-gray-400 items-center">
+                                            <div class="px-4 py-2 w-7/12"><img class="object-contain h-10 w-20 object-right" src={photo} /></div>
+                                            <div class="px-4 py-2 w-7/12">{foodName}</div>
+                                            <div class="px-4 py-2 w-7/12">{carb}</div>
+                                            <div class="px-4 py-2 w-7/12">{protein}</div>
+                                            <div class="px-4 py-2 w-7/12">{fat}</div>
+                                            <div class="px-4 py-2 w-7/12">{calorie}</div>
+                                            <div class="px-4 py-2 w-7/12">
+                                                <button class="text-gray-600 hover:text-black py-2 px-2 rounded-lg">
+                                                    <svg class="h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" /></svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </div>
+                    </div>
+                </div >
             </div>
-        </div >
+        </div>
     );
 }
 
