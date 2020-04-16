9e8993f25eedf042290ca997dd2f534fdad813ab
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-16 10:16:45 -0400

    Improve UserComputation UI

--- src/components/User/UserComputation.js
+++ src/components/User/UserComputation.js
@@ -7,12 +7,30 @@ import {
 function UserComputation({ age, height, weight, gender, selectedDay, days }) {
     return (
         <div class="h-full flex flex-col justify-center">
-            <div>BMR: {getBMR(age, height, weight, gender)}</div>
-            <div>Selected Day: {selectedDay}</div>
-            <div>Total Calories: {getTotal(days[selectedDay].map((meal) => (meal.foods.map((food) => (food.calorie)))).flat())}</div>
-            <div>Total Carbs: {getTotal(days[selectedDay].map((meal) => (meal.foods.map((food) => (food.carb)))).flat())}</div>
-            <div>Total Protein: {getTotal(days[selectedDay].map((meal) => (meal.foods.map((food) => (food.protein)))).flat())}</div>
-            <div>Total Fat: {getTotal(days[selectedDay].map((meal) => (meal.foods.map((food) => (food.fat)))).flat())}</div>
+            <div class="flex mb-6">
+                <p class="flex-1">BMR:</p>
+                <p>{getBMR(age, height, weight, gender) ?? "Incomplete Form"}</p>
+            </div>
+            <div class="flex">
+                <p class="flex-1">Selected Day:</p>
+                <p>{selectedDay}</p>
+            </div>
+            <div class="flex">
+                <p class="flex-1">Total Calories:</p>
+                <p>{getTotal(days[selectedDay].map((meal) => (meal.foods.map((food) => (food.calorie)))).flat())}</p>
+            </div>
+            <div class="flex">
+                <p class="flex-1">Total Carbs:</p>
+                <p>{getTotal(days[selectedDay].map((meal) => (meal.foods.map((food) => (food.carb)))).flat())}</p>
+            </div>
+            <div class="flex">
+                <p class="flex-1">Total Protein:</p>
+                <p>{getTotal(days[selectedDay].map((meal) => (meal.foods.map((food) => (food.protein)))).flat())}</p>
+            </div>
+            <div class="flex">
+                <p class="flex-1">Total Fat:</p>
+                <p>{getTotal(days[selectedDay].map((meal) => (meal.foods.map((food) => (food.fat)))).flat())}</p>
+            </div>
         </div>
     )
 }
