149c4f871a28172e68b17e4359295e65c7bc810c
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-17 17:33:24 -0400

    Added animation for list positioning

--- src/components/MealPlan/MealPlanItemList.js
+++ src/components/MealPlan/MealPlanItemList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MealPlanItem from './MealPlanItem';
 import { withRouter } from 'react-router-dom';
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 
 function MealPlanItemList({ meals, selectedDay, deleteMealActionCreator, addMealActionCreator, history, initialPosition, exitPosition }) {
     console.log(exitPosition);
@@ -12,25 +12,35 @@ function MealPlanItemList({ meals, selectedDay, deleteMealActionCreator, addMeal
             animate={{ x: 0 }}
             exit={{ x: exitPosition }}
             transition={{ ease: "easeInOut" }}>
+
+
             {
-                meals.map(({ mealName, foods }, index) => {
+                meals.map(({ mealName, foods, dateCreated }, index) => {
                     return (
-                        <div
+                        <motion.div
                             class="h-72 w-6/12 p-5"
-                            key={selectedDay + mealName + index}>
+                            positionTransition={{ ease: "easeInOut" }}
+                            initial={{ opacity: 0, y: 50, scale: 0.3 }}
+                            animate={{ opacity: 1, y: 0, scale: 1 }}
+                            transition={{ ease: "easeInOut" }}
+                            key={selectedDay + dateCreated}>
                             <MealPlanItem
                                 mealName={mealName}
                                 foods={foods}
                                 index={index}
                                 selectedDay={selectedDay}
                                 deleteMealActionCreator={deleteMealActionCreator} />
-                        </div>
+                        </motion.div>
                     );
                 })
             }
-            <div
+            <motion.div
                 class="h-72 w-6/12 p-5"
-                key={'/edit/' + selectedDay + '/' + meals.length}>
+                positionTransition={{ ease: "easeInOut" }}
+                initial={{ opacity: 0, y: 50, scale: 0.3 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                transition={{ ease: "easeInOut" }}
+                key={selectedDay}>
 
                 <button
                     class="bg-white rounded-lg shadow-lg w-full h-full flex flex-1 text-teal-400 hover:text-teal-700 justify-center"
@@ -42,8 +52,9 @@ function MealPlanItemList({ meals, selectedDay, deleteMealActionCreator, addMeal
                         <path d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 110-20 10 10 0 010 20zm0-2a8 8 0 100-16 8 8 0 000 16z" />
                     </svg>
                 </button>
-            </div>
-        </motion.div>
+            </motion.div>
+
+        </motion.div >
     );
 }
 
