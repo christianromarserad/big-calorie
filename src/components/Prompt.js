80994895f22a4089de0970022f545ff9f1266e1c
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-17 13:37:48 -0400

    Created Prompt component

---
+++ src/components/Prompt.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Modal from 'react-modal';
+import { motion, AnimatePresence } from "framer-motion"
+
+const Prompt = ({ isPromptOpen, children, styles }) => {
+
+    return (
+        <AnimatePresence>
+            {
+                isPromptOpen &&
+                <motion.div
+                    class="absolute top-0 right-0 h-full w-full bg-modal flex justify-center items-center"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}>
+
+                    <motion.div
+                        class={styles}
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        exit={{ scale: 0 }}>
+
+                        {children}
+
+                    </motion.div>
+                </motion.div>
+            }
+        </AnimatePresence>
+    );
+}
+
+export default Prompt;
